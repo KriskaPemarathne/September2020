917ae9e66898235a15b20d7da729e728dc61a746
kriskap <kriskap@gmail.com>
2020-10-05 12:12:22 +1300

    Define chrome driver as a static variable
    Define chrome driver as a static variable

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,57 +7,58 @@ namespace September2020
 {
     public class Program
     {
+       static IWebDriver driver;
         public static void Main(string[] args)
         {
 
-            IWebDriver driver = new ChromeDriver();
+             driver = new ChromeDriver();
             // Login page
             driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
             driver.Manage().Window.Maximize();
-
-            LoginToTurnUp(driver);
-            NavigateToHomePage(driver);
-            CreateNewTMTest(driver);
-            EditTMTest(driver);
-            DeleteTMTest(driver);
+            LoginToTurnUp();
+            NavigateToHomePage();
+            CreateNewTMTest();
+            EditTMTest();
+            DeleteTMTest();
 
 
         }
 
         [SetUp]
-        public static void LoginToTurnUp(IWebDriver driver)
+        public static void LoginToTurnUp()
         {
             Console.WriteLine("Hello World!");
             LoginPage loginObj = new LoginPage();
             loginObj.LoginSteps(driver);
         }
         [Test]
-        public static void NavigateToHomePage(IWebDriver driver)
+        public static void NavigateToHomePage()
         {
             HomePage homePage = new HomePage();
             homePage.NavigateToTM(driver);
         }
         [Test]
-        public static void CreateNewTMTest(IWebDriver driver)
+        public static void CreateNewTMTest()
         {
             TMPage tmPage = new TMPage();
             tmPage.CreateTM(driver);
         }
         [Test]
-        public static void EditTMTest(IWebDriver driver)
+        public static void EditTMTest()
         {
             TMPage tmPage = new TMPage();
             tmPage.EditTM(driver);
         }
         [Test]
-        public static void DeleteTMTest(IWebDriver driver)
+        public static void DeleteTMTest()
         {
             TMPage tmPage = new TMPage();
             tmPage.DeleteTM(driver);
         }
         [TearDown]
-        public void TestClosure(IWebDriver driver)
+        public void TestClosure()
         {
+            driver.Quit();
         }
     }
 
